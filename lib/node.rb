26de26f9a7307dcf27667c5f82f7dbdd8907d36e
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-09-10 14:52:02 +0100

    Set up Node class to build node.

--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,13 @@
 # node.rb
+# frozen_string_literal: true
 
+class Node
+  # attr_writer :value
+  # attr_reader :next_node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+end
